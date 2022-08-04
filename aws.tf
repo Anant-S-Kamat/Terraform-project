96375f68af43afc847aca3f33f14ef03f43bf372
Anant-S-Kamat <37654551+Anant-S-Kamat@users.noreply.github.com>
2022-08-04 10:58:54 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "Anant" {
   ami = "ami-068257025f72f470d"
-  instance type = "t2.micro"
+  instance_type = "t2.micro"
   count = 1
   subnet_id = "subnet-0d47c7cc17ec8f644"
   
   tags = {
-    Name = "Anant"
+    "Name" = "Anant"
   }
 }
